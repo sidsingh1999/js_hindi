9faf1a0b8c3fc9385a66d83c8d28f573e69d9c0b
sudhansu-si <sudhansu0599@gmail.com>
2026-06-12 10:53:24 +0530

    do it

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-console.log("India")
+//string concept in js 
+
+// const gameName = new String("Sudhansu-si");
+
+// console.log(gameName.length); // Output: 8
+// console.log(gameName.charAt(2)); // Output: "d"
+// console.log(gameName.toUpperCase()); 
+// console.log(gameName.indexOf('d')); 
+// this is string all methods
+
+// const newString = gameName.substring(0, 4); not giving negative value in substring method
+// console.log(newString); 
+
+// const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+const str1 = "   Hello   ";
+console.log(str1);
