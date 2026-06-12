1972c3380842989d877316b378467c5d98b80620
sudhansu-si <sudhansu0599@gmail.com>
2026-06-12 11:51:28 +0530

    commit thechnages

--- test.js
+++ test.js
@@ -46,3 +46,8 @@
 
 
 //++++++ Maths ++++++ // comes by default with js
+
+const hundreds = 1000000;
+console.log(Math);
+console.log(Math.abs(-5)); // Output: 5
+console.log(Math.round(3.7)); // Output: 4
